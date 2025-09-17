a662ac06c95f340eab0c715a65a6e8aac1560a92
Hdhillon2000 <hdhill63@my.centennialcollege.ca>
2025-09-17 19:17:47 -0400

    fixing file

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // React Router for navigation
-=======
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
->>>>>>> 54a17b0016ff063c3e9e3a9b5557134554c61ebf
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
-<<<<<<< HEAD
 import './App.css'; // Importing the CSS styles for the app
-=======
-import './App.css';
->>>>>>> 54a17b0016ff063c3e9e3a9b5557134554c61ebf
 
 function App() {
   return (
@@ -30,9 +22,5 @@ function App() {
   );
 }
 
-<<<<<<< HEAD
 export default App; // Exporting the App component to be used in other parts of the app
-=======
-export default App;
->>>>>>> 54a17b0016ff063c3e9e3a9b5557134554c61ebf
 
