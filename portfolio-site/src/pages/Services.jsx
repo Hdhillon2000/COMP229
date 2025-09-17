42650df15e2cffc64d42211562590f837e7806de
Hdhillon2000 <hdhill63@my.centennialcollege.ca>
2025-09-17 18:56:29 -0400

    Initial commit

---
+++ portfolio-site/src/pages/Services.jsx
@@ -0,0 +1,14 @@
+const Services = () => {
+  return (
+    <div>
+      <h2>Services I Offer</h2>
+      <ul>
+        <li>🌐 Web Development</li>
+        <li>📱 Mobile App Development</li>
+        <li>📈 UI/UX Design</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
