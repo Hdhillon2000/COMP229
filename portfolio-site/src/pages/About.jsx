37352802fdb15583ce5e2287d89eed252484d7d5
Hdhillon2000 <hdhill63@my.centennialcollege.ca>
2025-09-17 19:38:39 -0400

    fixing image path

--- portfolio-site/src/pages/About.jsx
+++ portfolio-site/src/pages/About.jsx
@@ -2,11 +2,11 @@ const About = () => {
   return (
     <div>
       <h2>About Me</h2>
-      <img src="public/my-photo.jpg.jpg" alt="My Photo" width="200" />
+      <img src="public/my-photo.jpg" alt="My Photo" width="200" />
       <p>Hi, I’m Harkaran Singh Dhillon, I’m a results-driven professional with over three years of experience in customer service, sales, and financial administration. With my IFIC certification, I’ve developed a solid foundation in financial services and a passion for delivering personalized solutions that meet customer needs. My approach to work is centered on building strong relationships and offering exceptional service, whether it’s helping clients navigate digital banking tools or providing expert financial advice. I take pride in consistently exceeding expectations and contributing to business growth in fast-paced environments.</p>
 <p>Outside of my professional life, I’m always looking for ways to grow and improve. I love hiking and exploring new places, experimenting with different cuisines, and practicing yoga to keep my mind and body in balance. I’m also dedicated to continuous learning, often diving into books or courses on personal finance and business strategies to stay ahead of trends.
 I believe in the importance of a well-rounded approach to both my career and personal life, always striving to be the best version of myself.</p>
-      <a href="public/resume.pdf.docx" target="_blank" rel="noopener noreferrer">
+      <a href="public/resume.docx" target="_blank" rel="noopener noreferrer">
         Download My Resume
       </a>
     </div>
